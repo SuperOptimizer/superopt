cf5f72c193db7c4ac75b89a4a3a19b4b5afb85bc
verditelabs <156155735+onedaybuildsfred@users.noreply.github.com>
2024-02-19 13:59:38 -0600

    install sentencepiece via apt

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 apt update
 apt upgrade -y
 apt dist-upgrade -y
-apt install gcc-riscv64-linux-gnu nano -y
+apt install gcc-riscv64-linux-gnu nano sentencepiece -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 mkdir -p /tmp/sopt
 mount -t tmpfs -o size=4096M tmpfs /tmp/sopt
